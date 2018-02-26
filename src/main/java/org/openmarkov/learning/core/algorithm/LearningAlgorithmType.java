ab3426ec344035a32578e73fe90293ee10564961
Jorge Pérez <jorgepmartin@gmail.com>
2018-02-26 10:56:14 +0100

    Modifying and including new license of OpenMarkov

--- src/main/java/org/openmarkov/learning/core/algorithm/LearningAlgorithmType.java
+++ src/main/java/org/openmarkov/learning/core/algorithm/LearningAlgorithmType.java
@@ -1,11 +1,9 @@
 /*
-* Copyright 2011 CISIAD, UNED, Spain
-*
-* Licensed under the European Union Public Licence, version 1.1 (EUPL)
-*
-* Unless required by applicable law, this code is distributed
-* on an "AS IS" basis, WITHOUT WARRANTIES OF ANY KIND.
-*/
+ * Copyright (c) CISIAD, UNED, Spain,  2018. Licensed under the GPLv3 licence
+ * Unless required by applicable law or agreed to in writing,
+ * this code is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTIES OF ANY KIND.
+ */
 
 package org.openmarkov.learning.core.algorithm;
 
