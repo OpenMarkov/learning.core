298b809e91124909f783083bbe9cf17ab8668320
Jorge Rico Vivas <jorgericoinformatic@gmail.com>
2026-03-16 13:06:24 +0100

    [UX, Performance] Tool plugin's from the "bnEvaluation" now save their dialogs, not requiring users to re-input every field on them. BNEvaluationDialog had minor adjustments for this to work (See the "reload" method). [UX] The user is now allowed to specify where the sets for "Split Dataset" will we saved at, as well as for the set resulting in "Data preprocessing". [UX] When opening/saving datasets, the extension of them are saved so the user gets them preselected when saving new datasets. If the dialog has a preselected file, then the extension taken by default will be that of the file. [UX, Bugfix] When using open/save dialogs made with OMFileChooser, the user no longer requires to manually add the extension of the file. In the past, even if he chose "Excel files.xslx" to save a a file, he had to add the extension on the name, now, if he writes "New file", then the extension will be added, turning it into "New file.xlsx". [UX] Cancel buttons (from the DialogBase) are now more recognizable and has been expanded to some DialogBase's dialogs that weren't using it (mostly dialogs from the "bnEvaluation" module, but also others, such as the Dialog for exceptions or the one for the Learning tool). [UI] Minor changes to interfaces (Replaced "Database" for "Dataset". Adjusted window dimensions and components positions. Corrected typos. Learning algorithms are shown in order in the Learning and Cross-Validation dialogs...). [Bugfix] Minor bugfixs on interfaces (Clicking "Open network from file" again in Bayesian Network Evaluation no longer removes the previously open network). [Bugfix] Solved the implementation of "equals" from the "State" class to take "Object" as parameter instead of "State". This makes the method "getStateIndex" of "Variable" to accept null values. [Bugfix] Solved a bug where the Learning dialog didn't allow to select variable for discriminative algorithms. [Feature, Development] Added a class (org.openmarkov.java.initialization.Lazy) for Lazy load of values. [Feature, Development] Added a class (org.openmarkov.gui.commonComponents.AutoResizedTableModel). [Feature, Development] Added utilities to create JTables out of a collection of rows, and another to save a JTable into a Sheet. Sheet are usually used in OpenMarkov to save data to an Excel's sheet. [Dependency] Updated vulnerable dependency of log4j-core to 2.25.3. [Refactor] Removed unused code from the "bnEvaluation" module. [Refactor] Cleaned some code. For example, OkCancelDialog's "getSelectedOption" now returns an enum, instead of an Integer. Dialog and BottomPanelButtonDialog's complexity has been reduced.

--- src/main/java/org/openmarkov/learning/core/algorithm/LearningAlgorithmManager.java
+++ src/main/java/org/openmarkov/learning/core/algorithm/LearningAlgorithmManager.java
@@ -9,6 +9,7 @@ package org.openmarkov.learning.core.algorithm;
 
 import org.jetbrains.annotations.NotNull;
 import org.openmarkov.core.exception.InvalidArgumentException;
+import org.openmarkov.java.function.PriorityComparator;
 import org.openmarkov.plugin.PluginSearch;
 
 import java.lang.reflect.InvocationTargetException;
